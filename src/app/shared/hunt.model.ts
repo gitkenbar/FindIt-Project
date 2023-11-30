0f5ce3b98808b0a2fc0182ebb0143562998ed31f
gitkenbar <kennethbarker12@gmail.com>
2023-11-30 14:32:10 -0600

    Added Modal CSS, adjusted create-hunts.html,  added half of Auth Component(WIP)

--- src/app/shared/hunt.model.ts
+++ src/app/shared/hunt.model.ts
@@ -6,11 +6,7 @@ export class Hunt {
     public name:string,             //Name or Title of a given scavenger hunt
     public begin:Date,              //The time that the scavenger hunt begins
     public end:Date,                //The time the scavenger hunt ends
-<<<<<<< Updated upstream
-    public itemList:Item[],         //Array containing the list of Items (see item.model.ts) to be found
-=======
     public itemList:Item[],      //Array containing the list of Items (see item.model.ts) to be found
->>>>>>> Stashed changes
     public isProtected:Boolean,     //As per Kenneth, the option to make hunts private
     private passkey?:string         //to be used ifProtected=true as a passkey to grant user access to a hunt
   ) {}
