baecbb6fbe64bd0afa6eff81a7a91e56789cf96c
PatrickRanval <pat.ranval@gmail.com>
2023-12-08 12:48:40 -0600

    Pretty big Updates to CreateHunts for itemized Item adding
    Significant update to template to support dynamic adding of Items

--- src/app/create-hunts/create-hunts.component.ts
+++ src/app/create-hunts/create-hunts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hunt } from '../shared/hunt.model';
+import { Item } from '../shared/item.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { HuntService } from '../shared/hunt.service';
@@ -12,6 +13,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 })
 export class CreateHuntsComponent {
   isEditingHunt: boolean = false;
+  selectedProof:string = 'option1';
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -21,18 +23,29 @@ export class CreateHuntsComponent {
   onFormSubmit(form: NgForm) {
     console.log(form);
     if (form.invalid) return;
+    const uid = Date.now();  //lazy implementation of Unique-enough-IDs
+    const name = form.value.name;
+    const begin = form.value.begin;
+    const end = form.value.end;
+    const itemList: Item[] = [
+    new Item('ducks', false),
+    new Item('fish', false)
+    ]
     // const { uid, name, begin, end, itemList } = Hunt  //destructuring example if needed
-    const newHunt = new Hunt(  //Consider destructuring as alternative solution for clarity
-      +(Math.random() * 1000).toFixed(0),
-      form.value.name,
-      form.value.begin,
-      form.value.end,
-      form.value.itemList.split(" "),
-      false )
+    const newHunt = new Hunt(uid, name, begin, end, itemList, false)
     this.huntService.addHunt(newHunt)
     this.onResetForm(form);
     console.log(newHunt);
     this.dbService.saveToDB();
+  }
+
+  getSelectedProof() {
+    this.selectedProof;
+    console.log(this.selectedProof);
+  }
+
+  addNewItem() {
+
   }
 
   onResetForm(form?: NgForm) {
