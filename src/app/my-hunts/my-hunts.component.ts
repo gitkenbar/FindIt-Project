3770a2f002cf8d0a9b54f1ee77d11a5fafa9cffd
JeredHuck <jeredhuck@gmail.com>
2023-12-15 17:42:55 -0600

    item check off, my hunts page beautification

--- src/app/my-hunts/my-hunts.component.ts
+++ src/app/my-hunts/my-hunts.component.ts
@@ -45,13 +45,28 @@ export class MyHuntsComponent implements OnInit, OnDestroy{
   }
 
   ngOnDestroy(): void {
-    this.savedHuntsSub.unsubscribe();
     if(this.selectedHuntSub) {
       this.selectedHuntSub.unsubscribe();
     }
     if(this.displayedHuntsSub) {
       this.displayedHuntsSub.unsubscribe();
     }
+  }
+
+  onFind(i: number) {
+    this.selectedHunt.itemList[i].huntStatus = !this.selectedHunt.itemList[i].huntStatus;
+    console.log(this.selectedHunt.itemList);
+  }
+
+  onComplete() {
+    let itemList = this.selectedHunt.itemList
+    for (const item of itemList) {
+      if (item.huntStatus) {
+
+        // Perform actions for valid items here
+      }
+    }
+    return false;
   }
 
   // //Debug Object
--- src/app/shared/hunt.service.ts
+++ src/app/shared/hunt.service.ts
@@ -8,9 +8,9 @@ import { Item } from './item.model';
 })
 export class HuntService {
   debugItems:Item[] = [
-    new Item('Bugs', true),
+    new Item('Bugs', false),
     new Item('Duck', false),
-    new Item('Goose', true)
+    new Item('Goose', false)
     ]
 
     debugHunt = new Hunt(
@@ -21,11 +21,34 @@ export class HuntService {
       this.debugItems,
       false
     )
+
+    testItems: Item[] = [
+      new Item('Oak Tree', false),
+      new Item('Poplar Tree', false),
+      new Item('Sassafras Tree', false)
+    ]
+
+    testHunt = new Hunt(
+      0,
+      'Test Hunt',
+      new Date(),
+      new Date(3),
+      this.testItems,
+      false
+    )
   huntsChanged = new Subject<Hunt[]>()
   huntSelected = new BehaviorSubject<Hunt>(null);   //flexible observable to broadcast a selected hunt
 
   private myHunts: Hunt[] = [
     // new Hunt(12345, 'The Great Hunt', new Date, new Date, [], false, 'elgato')
+    this.debugHunt,
+    this.debugHunt,
+    this.debugHunt,
+    this.debugHunt,
+    this.testHunt,
+    this.debugHunt,
+    this.testHunt,
+    this.debugHunt,
     this.debugHunt
   ]  //propose renaming variable to reduce ambiguity with GlobalHuntService ::: Maybe we could call them "mySavedHunts"?
 
@@ -34,6 +57,14 @@ export class HuntService {
   addHunt(newHunt: Hunt) {
     this.myHunts.push(newHunt);
     console.log(this.myHunts);
+  }
+
+  deleteHuntById(uid: number) {
+    const hunts = this.myHunts.filter((hunt) => hunt.uid !== uid);
+
+    console.log(hunts);
+    this.myHunts = hunts;
+    this.huntsChanged.next(this.myHunts.slice());
   }
 
   getMyHunts() {
