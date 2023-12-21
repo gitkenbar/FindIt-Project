ea333b807db1d5e2ae5659799c22edca5b974a79
PatrickRanval <pat.ranval@gmail.com>
2023-12-21 15:17:22 -0600

    Some pruning
    See above

--- src/app/shared/hunt.model.ts
+++ src/app/shared/hunt.model.ts
@@ -6,7 +6,7 @@ export class Hunt {
     public name:string,             //Name or Title of a given scavenger hunt
     public begin:Date,              //The time that the scavenger hunt begins
     public end:Date,                //The time the scavenger hunt ends
-    public itemList:Item[],      //Array containing the list of Items (see item.model.ts) to be found
+    public itemList:Item[],         //Array containing the list of Items (see item.model.ts) to be found
     public isProtected:Boolean,     //As per Kenneth, the option to make hunts private
     //public adminID:string,        //Field for holding AdminID to be implemented
     //public participatingIDs:string[],  //Field for participating UserIDs to be implemented
--- src/app/shared/hunt.service.ts
+++ src/app/shared/hunt.service.ts
@@ -1,45 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Hunt } from './hunt.model';
 import { BehaviorSubject} from 'rxjs';
-import { Item } from './item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HuntService {
-  debugItems:Item[] = [
-    new Item('Bugs', false),
-    new Item('Duck', false),
-    new Item('Goose', false)
-    ]
 
-    debugHunt = new Hunt(
-      79,
-      'Debug Hunt',
-      new Date(),
-      new Date(7),
-      this.debugItems,
-      false
-    )
-
-    testItems: Item[] = [
-      new Item('Oak Tree', false),
-      new Item('Poplar Tree', false),
-      new Item('Sassafras Tree', false)
-    ]
-
-    testHunt = new Hunt(
-      1,
-      'Test Hunt',
-      new Date(),
-      new Date(3),
-      this.testItems,
-      false
-    )
-
-    huntSelected = new BehaviorSubject<Hunt>(null);
-    huntsChanged = new BehaviorSubject<Hunt[]>(null);
-    trophyCase = new BehaviorSubject<string[]>(null);
+  huntSelected = new BehaviorSubject<Hunt>(null);
+  huntsChanged = new BehaviorSubject<Hunt[]>(null);
+  trophyCase = new BehaviorSubject<string[]>(null);
 
   private myHunts: Hunt[] = [];
   private myTrophies: string[] = [];
@@ -74,14 +44,6 @@ export class HuntService {
     this.trophyCase.next(this.myTrophies.slice())
   }
 
-  // setMyTrophies(myTrophies: string[]) {
-  //   this.myTrophies = myTrophies;
-  //   this.trophyCase.next(this.myTrophies.slice())
-  // }
-
-  // getMyTrophies() {
-  //   return this.myTrophies.slice()
-  // }
 
   setHuntSelectedByIndex(uid: number) {
     const selectedHunt = this.myHunts.find((hunt) => hunt.uid === uid)
@@ -90,3 +52,41 @@ export class HuntService {
   }
 
 }
+    // debugItems:Item[] = [
+    //   new Item('Bugs', false),
+    //   new Item('Duck', false),
+    //   new Item('Goose', false)
+    //   ]
+
+    //   debugHunt = new Hunt(
+    //     79,
+    //     'Debug Hunt',
+    //     new Date(),
+    //     new Date(7),
+    //     this.debugItems,
+    //     false
+    //   )
+
+    //   testItems: Item[] = [
+    //     new Item('Oak Tree', false),
+    //     new Item('Poplar Tree', false),
+    //     new Item('Sassafras Tree', false)
+    //   ]
+
+    //   testHunt = new Hunt(
+    //     1,
+    //     'Test Hunt',
+    //     new Date(),
+    //     new Date(3),
+    //     this.testItems,
+    //     false
+    //   )
+
+    // setMyTrophies(myTrophies: string[]) {
+    //   this.myTrophies = myTrophies;
+    //   this.trophyCase.next(this.myTrophies.slice())
+    // }
+
+    // getMyTrophies() {
+    //   return this.myTrophies.slice()
+    // }
